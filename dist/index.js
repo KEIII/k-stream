8fb43a96bc837f9ba5605bac5c409d7bbaf0902e
Ivan Kasenkov <kacenkov7@gmail.com>
2020-04-20 12:10:12 +0300

    setup ts build

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./core"));
+__export(require("./factories"));
+__export(require("./subject"));
+__export(require("./transformers"));
+__export(require("./ts-option"));
+__export(require("./ts-result"));
+//# sourceMappingURL=index.js.map
--- index.js
+++
@@ -1,6 +0,0 @@
-export * from "./src/core";
-export * from "./src/factories";
-export * from "./src/subject";
-export * from "./src/transformers";
-export * from "./src/ts-option";
-export * from "./src/ts-result";
