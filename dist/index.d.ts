7f3d997f985c99f904d8c4ce9965379c341a5ab9
Ivan Kasenkov <kacenkov7@gmail.com>
2020-08-08 19:59:56 +0300

    deploying to npm

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from './core';
-export * from './factories';
-export * from './behaviourSubject';
-export * from './subject';
-export * from './transformers';
-export * from './ts-option';
-export * from './ts-result';
--- dist/subject.d.ts
+++
@@ -1,6 +0,0 @@
-import { CompleteFn, Stream } from './core';
-export declare type Subject<T> = Stream<T> & {
-    readonly next: (value: T) => void;
-    readonly complete: CompleteFn;
-};
-export declare const ksSubject: <T>(behaviour?: import("./core").KsBehaviour) => Subject<T>;
