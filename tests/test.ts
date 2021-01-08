f7c15f4dd6a3729caa1517a617aa97e7fdead5c7
Ivan Kasenkov <kacenkov7@gmail.com>
2021-01-08 13:02:58 +0300

    performance test

--- tests/test.ts
+++ tests/test.ts
@@ -733,3 +733,29 @@ describe('pipe', () => {
     expect((await stackOut(s))[0]).toBe('16');
   });
 });
+
+describe('performance', () => {
+  it('maximum call stack size exceeded', () => {
+    const m = ksMap((x: number) => x + 1);
+    let s = ksOf(0);
+    const max = 2_576;
+    for (let i = 0; i < max; i++) {
+      s = s.pipe(m);
+    }
+    let result = 0;
+    s.subscribe({ next: value => (result = value) }).unsubscribe();
+    expect(result).toBe(max);
+  });
+
+  it('rxjs: maximum call stack size exceeded', () => {
+    const m = map((x: number) => x + 1);
+    let s = of(0);
+    const max = 1_000;
+    for (let i = 0; i < max / 8; i++) {
+      s = s.pipe(m, m, m, m, m, m, m, m);
+    }
+    let result = 0;
+    s.subscribe({ next: value => (result = value) }).unsubscribe();
+    expect(result).toBe(max);
+  });
+});
