8caa3fe07708e248d2a677f45d84ed72fd7fe679
Ivan Kasenkov <kacenkov7@gmail.com>
2021-09-05 20:20:39 +0300

    test: monad laws

--- tests/test.ts
+++ tests/test.ts
@@ -66,6 +66,7 @@ import {
   ksRetryWhen,
   ksWithLatestFrom,
   ksAudit,
+  Stream,
 } from '../src';
 
 const stackOut = <A>(observable: {
@@ -1237,5 +1238,33 @@ describe('diamond problem (glitches)', () => {
       'g36',
       'h49',
     ]);
+  });
+});
+
+describe('Monad laws', () => {
+  const equivalence = async <T>(a: Stream<T>, b: Stream<T>) => {
+    expect(await stackOut(a)).toEqual(await stackOut(b));
+  };
+
+  it('Left identity', async () => {
+    const a = Math.random();
+    const ma = ksOf(a);
+    const f = (x: number) => ksOf(x * 2);
+    await equivalence(ma.pipe(ksSwitch(f)), f(a));
+  });
+
+  it('Right identity', async () => {
+    const ma = ksOf(Math.random());
+    await equivalence(ma.pipe(ksSwitch(ksOf)), ma);
+  });
+
+  it('Associativity', async () => {
+    const ma = ksOf(2);
+    const f = (x: number) => ksOf(x * 4);
+    const g = (x: number) => ksOf(x * 6);
+    await equivalence(
+      ma.pipe(ksSwitch(f)).pipe(ksSwitch(g)),
+      ma.pipe(ksSwitch(x => f(x).pipe(ksSwitch(g)))),
+    );
   });
 });
